4efc69413a160aa8f377d374a16cf04ceccac82b
Eduardo Moreira (Moreira) <edmoreira043557@gmail.com>
2024-12-15 05:51:45 -0300

    Continuidade das aulas

---
+++ assets/js/JavaScript.js
@@ -0,0 +1,28 @@
+$(function(){
+    clickMenu();
+    scrollItem();
+    function clickMenu() {
+        $('#menu-principal a, .list-group-item a').click(function() {
+            $('#menu-principal a, .list-group-item a').removeClass('active');
+            $('#menu-principal a').parent().removeClass('active');
+            $('#menu-principal a[ref_sys='+ $(this).attr('ref_sys') +']').parent().addClass('active');
+            $('.list-group-item a[ref_sys='+ $(this).attr('ref_sys') +']').addClass('active');
+            return false;
+        })
+    }
+
+    function scrollItem() {
+        $('#menu-principal a, .list-group-item a').click(function() {
+            var ref = '#' + $(this).attr('ref_sys') + '_section';
+            var offset = $(ref).offset().top;
+            $('html, body').animate({'scrollTop': offset}, top-50);
+        });
+    }
+
+    $('#menu-principal a').click(function() {
+        if ($(window).width() < 768) {
+            $('.navbar-toggler-icon').click()
+        }
+    })
+
+})
